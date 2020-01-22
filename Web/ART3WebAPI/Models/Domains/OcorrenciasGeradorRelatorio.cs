ad8e5e18a7bfcea72812a62f0c5de216adb6ffbf
Luiz Xavier <luiz.xavier@tropikal.com.br>
2020-01-22 00:36:25 -0300

    alterados 21-01-2020

---
+++ Web/ART3WebAPI/Models/Domains/OcorrenciasGeradorRelatorio.cs
@@ -0,0 +1,185 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ART3WebAPI.Models.Entities;
+using ART3WebAPI.Models.Repository;
+using ART3WebAPI.Models.Domains;
+using System;
+
+namespace ART3WebAPI.Models.Domains
+{
+    public class OcorrenciasGeradorRelatorio
+    {
+        #region [ Constantes ]
+        private const int LIN_INICIO_REGISTROS = 8;
+        private const int LIN_CABECALHO = 7;
+        private const int COL_Loja = 2;
+        private const int COL_CD = 3;
+        private const int COL_Pedido = 4;
+        private const int COL_NF = 5;
+        private const int COL_Cliente = 6;
+        private const int COL_UF = 7;
+        private const int COL_Cidade = 8;
+        private const int COL_Transportadora = 9;
+        private const int COL_Contato = 10;
+        private const int COL_Telefone = 11;
+        private const int COL_Ocorrencia = 12;
+        private const int COL_TipoOcorrencia = 13;
+        private const int COL_Status = 14;
+        #endregion
+
+        public static Task GenerateXLS(List<OcorrenciasStatus> datasource, string filePath, string oc_status, string loja, string transportadora)
+        {
+            return Task.Run(() =>
+            {
+                int NumRegistros = datasource.Count;
+
+                if (string.IsNullOrEmpty(oc_status))
+                    oc_status = "Ambos";
+                if ((string.IsNullOrEmpty(transportadora)) || (transportadora == "0"))
+                    transportadora = "Todos";
+                if (string.IsNullOrEmpty(loja))
+                    loja = "Todas";
+                using (ExcelPackage pck = new ExcelPackage())
+                {
+                    //Cria uma planilha com nome
+                    ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Ocorrências ");
+
+                    #region [ Config Gerais ]
+                    //configurações gerais da planilha
+                    ws.Cells["A:XFD"].Style.Font.Name = "Arial";
+                    ws.Cells["A:XFD"].Style.Font.Size = 10;
+                    ws.View.ShowGridLines = false;
+
+
+                    ws.Column(1).Width = 2;
+                    ws.Column(COL_Loja).Width = 15;
+                    ws.Column(COL_CD).Width = 15;
+                    ws.Column(COL_Pedido).Width = 15;
+                    ws.Column(COL_NF).Width = 15;
+                    ws.Column(COL_Cliente).Width = 40;
+					ws.Column(COL_UF).Width = 15;
+					ws.Column(COL_Cidade).Width = 40;
+					ws.Column(COL_Transportadora).Width = 20;
+					ws.Column(COL_Contato).Width = 20;
+                    ws.Column(COL_Telefone).Width = 20;
+					ws.Column(COL_Ocorrencia).Width = 40;
+                    ws.Column(COL_TipoOcorrencia).Width = 40;
+					ws.Column(COL_Status).Width = 20;
+ 
+                    ws.Row(1).Height = 1;                
+                    #endregion
+
+
+                    #region [ Filtro ]
+                    ws.Cells["B2:M12"].Style.Font.Bold = true;
+                    ws.Cells["B2"].Style.Font.Size = 12;
+                    ws.Cells["B2"].Value = "Ocorrências ";
+                    ws.Cells["B3"].Value = "Transportadora: " + transportadora;
+                    ws.Cells["B4"].Value = "Loja: " + loja;
+                    ws.Cells["B5"].Value = "Emissão: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+                    #endregion
+
+                    #region [ Cabeçalho ]
+                    using (ExcelRange rng1 = ws.Cells["B" + (LIN_INICIO_REGISTROS - 1) + ":" + ws.Cells[LIN_INICIO_REGISTROS - 1, COL_Status]])
+                    {
+                        rng1.Style.WrapText = true;
+                        rng1.Style.Font.Bold = true;
+                        rng1.Style.VerticalAlignment = ExcelVerticalAlignment.Bottom;
+                        rng1.Style.Border.Top.Style = ExcelBorderStyle.Medium;
+                        rng1.Style.Border.Bottom.Style = ExcelBorderStyle.Medium;
+                        rng1.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                    }
+
+                    ws.Cells[LIN_CABECALHO, COL_TipoOcorrencia].Style.Border.Right.Style = ExcelBorderStyle.Medium;
+                    ws.Cells[LIN_CABECALHO, COL_Loja].Value = "Loja";
+                    ws.Cells[LIN_CABECALHO, COL_CD].Value = "CD";
+                    ws.Cells[LIN_CABECALHO, COL_Pedido].Value = "Pedido";
+                    ws.Cells[LIN_CABECALHO, COL_NF].Value = "NF";
+                    ws.Cells[LIN_CABECALHO, COL_Cliente].Value = "Cliente";
+                    ws.Cells[LIN_CABECALHO, COL_UF].Value = "Estado";
+                    ws.Cells[LIN_CABECALHO, COL_Cidade].Value = "Cidade";
+                    ws.Cells[LIN_CABECALHO, COL_Transportadora].Value = "Transportadora";
+                    ws.Cells[LIN_CABECALHO, COL_Contato].Value = "Contato";
+                    ws.Cells[LIN_CABECALHO, COL_Telefone].Value = "Telefone";
+                    ws.Cells[LIN_CABECALHO, COL_Ocorrencia].Value = "Ocorrência";
+                    ws.Cells[LIN_CABECALHO, COL_TipoOcorrencia].Value = "Tipo de Ocorrência";
+					ws.Cells[LIN_CABECALHO, COL_Status].Value = "Status";                    
+
+
+                    ws.Cells[LIN_CABECALHO, COL_Loja].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                    ws.Cells[LIN_CABECALHO, COL_CD].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                    ws.Cells[LIN_CABECALHO, COL_Pedido].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                    ws.Cells[LIN_CABECALHO, COL_NF].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                    ws.Cells[LIN_CABECALHO, COL_Cliente].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                    ws.Cells[LIN_CABECALHO, COL_UF].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                    ws.Cells[LIN_CABECALHO, COL_Cidade].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                    ws.Cells[LIN_CABECALHO, COL_Transportadora].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                    ws.Cells[LIN_CABECALHO, COL_Contato].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                    ws.Cells[LIN_CABECALHO, COL_Telefone].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                    ws.Cells[LIN_CABECALHO, COL_Ocorrencia].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                    ws.Cells[LIN_CABECALHO, COL_TipoOcorrencia].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                    ws.Cells[LIN_CABECALHO, COL_Status].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                    #endregion
+
+                    #region [ Registros ]
+                    using (ExcelRange rng2 = ws.Cells["B" + LIN_INICIO_REGISTROS + ":" + ws.Cells[NumRegistros + LIN_INICIO_REGISTROS - 1, COL_Status]])
+                    {
+                        rng2.Style.Border.Bottom.Style = ExcelBorderStyle.Hair;
+                        rng2.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                        rng2.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                    }
+
+                    for (int i = 0; i < NumRegistros; i++)
+                    {
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_Loja].Value = datasource.ElementAt(i).Loja;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_Loja].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_CD].Value = datasource.ElementAt(i).CD;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_CD].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_Pedido].Value = datasource.ElementAt(i).Pedido;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_Pedido].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_NF].Value = datasource.ElementAt(i).NF;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_NF].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_Cliente].Value = datasource.ElementAt(i).Cliente;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_Cliente].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+						ws.Cells[i + LIN_INICIO_REGISTROS, COL_Cliente].Style.WrapText = true;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_UF].Value = datasource.ElementAt(i).UF;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_UF].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_Cidade].Value = datasource.ElementAt(i).Cidade;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_Cidade].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_Transportadora].Value = datasource.ElementAt(i).Transportadora;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_Transportadora].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_Contato].Value = datasource.ElementAt(i).Contato;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_Contato].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_Telefone].Value = datasource.ElementAt(i).Telefone;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_Telefone].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_Ocorrencia].Value = datasource.ElementAt(i).Ocorrencia;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_Ocorrencia].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_TipoOcorrencia].Value = datasource.ElementAt(i).TipoOcorrencia;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_TipoOcorrencia].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_TipoOcorrencia].Style.WrapText = true;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_Status].Value = datasource.ElementAt(i).Status;
+                        ws.Cells[i + LIN_INICIO_REGISTROS, COL_Status].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                        
+                    }
+                    #endregion
+
+
+                    #region [ Total ]
+
+                    ws.Cells[(NumRegistros + LIN_INICIO_REGISTROS + 1), COL_Loja, (NumRegistros + LIN_INICIO_REGISTROS + 1), COL_Status].Style.Font.Bold = true;
+                    ws.Cells[NumRegistros + LIN_INICIO_REGISTROS + 1, COL_Loja].Value = "TOTAL:";
+                    ws.Cells[NumRegistros + LIN_INICIO_REGISTROS + 1, COL_Loja].Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+                    ws.Cells[NumRegistros + LIN_INICIO_REGISTROS + 1, COL_CD].Value = NumRegistros + " Ocorrência(s)";
+                    #endregion
+
+                    pck.SaveAs(new FileInfo(filePath));
+                }
+            });
+        }
+    }
+}
---
+++ Web/ART3WebAPI/Models/Repository/DataOcorrencias.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using ART3WebAPI.Models.Entities;
+using ART3WebAPI.Models.Domains;
+using ART3WebAPI.Models.Repository;
+using System;
+
+namespace ART3WebAPI.Models.Repository
+{
+    public class DataOcorrencias
+    {
+        public OcorrenciasStatus[] Get(string oc_status,string transportadora,string loja)
+        {
+            List<OcorrenciasStatus> ListaOcorrenciasStatus = new List<OcorrenciasStatus>();
+            SqlConnection cn = new SqlConnection(BD.getConnectionString());
+            
+            
+            //string s_where_temp, sqlString, s;
+            string sqlString;
+
+
+
+            #region [s_where e Consulta]
+
+            sqlString = "SELECT" +
+						" tPO.id," +
+						" tPO.pedido," +
+						" tPO.usuario_cadastro," +
+						" tPO.dt_cadastro," +
+						" tPO.dt_hr_cadastro," +
+						" tPO.contato," +
+						" tPO.ddd_1," +
+						" tPO.tel_1," +
+						" tPO.ddd_2," +
+						" tPO.tel_2," +
+                        "'('+ tPO.ddd_1+')'+tPO.tel_1 AS telefone," +
+                        " tPO.texto_ocorrencia," +
+                        " tPO.tipo_ocorrencia," +
+                        " tP.loja," +
+						" tP.loja AS pedido_loja," +
+						" tP.transportadora_id," +
+						" tC.nome_iniciais_em_maiusculas AS nome_cliente, tCD.codigo, tCD.descricao," +
+						" tEmit.apelido as CD, " +
+                        " CASE WHEN tP.st_end_entrega <> 0 THEN tP.EndEtg_uf ELSE tC.uf END AS UF, " +
+                        " CASE WHEN tP.st_end_entrega <> 0 THEN tP.EndEtg_cidade ELSE tC.cidade END AS Cidade, " +
+						" (" +
+							"SELECT" +
+								" TOP 1 NFe_numero_NF" +
+							" FROM t_NFe_EMISSAO tNE" +
+							" WHERE" +
+								" (tNE.pedido=tPO.pedido)" +
+								" AND (tipo_NF = '1')" +
+								" AND (st_anulado = 0)" +
+								" AND (codigo_retorno_NFe_T1 = 1)" +
+							" ORDER BY" +
+								" id DESC" +
+						") AS numeroNFe," +
+						" (" +
+							"SELECT" +
+								" Count(*)" +
+							" FROM t_PEDIDO_OCORRENCIA_MENSAGEM" +
+							" WHERE" +
+								" (id_ocorrencia=tPO.id)" +
+								" AND (fluxo_mensagem='" + Global.Cte.COD_FLUXO_MENSAGEM_OCORRENCIAS_EM_PEDIDOS__CENTRAL_PARA_LOJA + "')" +
+						") AS qtde_msg_central," +
+						" (" +
+							" SELECT Count(*)" +
+							   " FROM t_PEDIDO_OCORRENCIA_MENSAGEM INNER JOIN t_PEDIDO_OCORRENCIA ON (t_PEDIDO_OCORRENCIA_MENSAGEM.id_ocorrencia=t_PEDIDO_OCORRENCIA.id)" +
+							   " INNER JOIN t_PEDIDO ON (t_PEDIDO_OCORRENCIA.pedido=t_PEDIDO.pedido)" + 
+							   " WHERE (id_ocorrencia = tPO.id)" +
+							   " AND (t_PEDIDO.loja = '" + Global.Cte.Loja.ArClube + "')" + 
+						") AS qtde_msg," +
+						" CASE WHEN (" +
+                                     " (" +
+                                        "SELECT" +
+                                            " Count(*)" +
+                                        " FROM t_PEDIDO_OCORRENCIA_MENSAGEM" +
+                                        " WHERE" +
+                                            " (id_ocorrencia=tPO.id)" +
+                                            " AND (fluxo_mensagem='" + Global.Cte.COD_FLUXO_MENSAGEM_OCORRENCIAS_EM_PEDIDOS__CENTRAL_PARA_LOJA + "')" +
+                                    ") = 0 AND " +
+                                    " (" +
+                                        " SELECT Count(*)" +
+                                           " FROM t_PEDIDO_OCORRENCIA_MENSAGEM INNER JOIN t_PEDIDO_OCORRENCIA ON (t_PEDIDO_OCORRENCIA_MENSAGEM.id_ocorrencia=t_PEDIDO_OCORRENCIA.id)" +
+                                           " INNER JOIN t_PEDIDO ON (t_PEDIDO_OCORRENCIA.pedido=t_PEDIDO.pedido)" +
+                                           " WHERE (id_ocorrencia = tPO.id)" +
+                                           " AND (t_PEDIDO.loja = '" + Global.Cte.Loja.ArClube + "')" +
+                                    ") = 0" +
+                        ") THEN 'Aberta' ELSE 'Em Andamento' END AS status_ocorrencia" +
+				   " FROM t_PEDIDO_OCORRENCIA tPO" +
+						" INNER JOIN t_PEDIDO tP ON (tPO.pedido=tP.pedido)" +
+						" INNER JOIN t_CLIENTE tC ON (tP.id_cliente=tC.id)" +
+						" INNER JOIN t_NFe_EMITENTE tEmit ON (tP.id_nfe_emitente=tEmit.id)" +
+						" LEFT JOIN t_CODIGO_DESCRICAO tCD ON (tPO.cod_motivo_abertura=tCD.codigo) AND (tCD.grupo='" + Global.Cte.GRUPO_T_CODIGO_DESCRICAO__OCORRENCIAS_EM_PEDIDOS__MOTIVO_ABERTURA + "')" +
+				   " WHERE" +
+						" (finalizado_status = 0)";
+						
+						
+					if ((!string.IsNullOrEmpty(transportadora)) && (transportadora != "0"))
+					{						
+						sqlString = sqlString + " AND (tP.transportadora_id = < " + transportadora + ")";					
+					}
+
+					if (!string.IsNullOrEmpty(loja))
+					{
+						sqlString = sqlString + " AND (tP.numero_loja = < " + loja + ")";					
+					}
+
+					if (oc_status == "ABERTA") 
+					{
+						sqlString = "SELECT * FROM (" + sqlString + ") t WHERE (qtde_msg_central = 0 AND qtde_msg = 0) ORDER BY dt_hr_cadastro, id";
+					}
+					else if (oc_status == "EM_ANDAMENTO")
+					{
+						sqlString = "SELECT * FROM (" + sqlString + ") t WHERE (qtde_msg_central > 0 OR qtde_msg > 0) ORDER BY dt_hr_cadastro, id";
+					}
+					else
+					{
+						sqlString = "SELECT * FROM (" + sqlString + ") t ORDER BY dt_hr_cadastro, id";
+					}
+
+            #endregion
+            
+			cn.Open();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = sqlString.ToString();
+                IDataReader reader = cmd.ExecuteReader();
+
+                try
+                {
+                    int idxLoja = reader.GetOrdinal("loja");
+                    int idxCD = reader.GetOrdinal("CD");
+                    int idxPedido = reader.GetOrdinal("pedido");
+                    int idxNF = reader.GetOrdinal("numeroNFe");
+                    int idxCliente = reader.GetOrdinal("nome_cliente");
+                    int idxUF = reader.GetOrdinal("UF");
+                    int idxCidade = reader.GetOrdinal("cidade");
+                    int idxTransportadora = reader.GetOrdinal("transportadora_id");
+                    int idxContato = reader.GetOrdinal("contato");
+                    int idxTelefone = reader.GetOrdinal("telefone");
+                    int idxOcorrencia = reader.GetOrdinal("texto_ocorrencia");
+                    int idxTipoOcorrencia = reader.GetOrdinal("tipo_ocorrencia");
+                    int idxStatus = reader.GetOrdinal("status_ocorrencia");
+
+                    while (reader.Read())
+                    {
+                        string consulta;
+                        OcorrenciasStatus _novo = new OcorrenciasStatus();
+                        _novo.Loja = reader.IsDBNull(idxLoja) ? "" : reader.GetString(idxLoja);
+						_novo.CD = reader.IsDBNull(idxCD) ? "" : reader.GetString(idxCD);
+						_novo.Pedido = reader.IsDBNull(idxPedido) ? "" : reader.GetString(idxPedido);
+                        _novo.NF = reader.IsDBNull(idxNF) ? 0 : reader.GetInt32(idxNF);
+                        _novo.Cliente = reader.IsDBNull(idxCliente) ? "" : reader.GetString(idxCliente);
+						_novo.UF = reader.IsDBNull(idxUF) ? "" : reader.GetString(idxUF);
+						_novo.Cidade = reader.IsDBNull(idxCidade) ? "" : reader.GetString(idxCidade);
+                        _novo.Transportadora = reader.IsDBNull(idxTransportadora) ? "" : reader.GetString(idxTransportadora);
+						_novo.Contato = reader.IsDBNull(idxContato) ? "" : reader.GetString(idxContato);
+						_novo.Telefone = reader.IsDBNull(idxTelefone) ? "" : reader.GetString(idxTelefone);
+						_novo.Ocorrencia = reader.IsDBNull(idxOcorrencia) ? "" : reader.GetString(idxOcorrencia);
+						_novo.TipoOcorrencia = reader.IsDBNull(idxTipoOcorrencia) ? "" : reader.GetString(idxTipoOcorrencia);
+						_novo.Status = reader.IsDBNull(idxStatus) ? "" : reader.GetString(idxStatus);
+
+                        /* consulta = BD.obtem_descricao_tabela_t_codigo_descricao(Global.Cte.GRUPO_T_CODIGO_DESCRICAO__OCORRENCIAS_EM_PEDIDOS__MOTIVO_ABERTURA, reader.IsDBNull(idxCod_motivo_abertura) ? "" : reader.GetString(idxCod_motivo_abertura));
+                        if (!string.IsNullOrEmpty(consulta))
+                        {
+                            _novo.Ocorrencia = consulta + " " + reader.GetString(idxOcorrencia);
+                            
+                        }
+                        else
+                        {
+                            _novo.Ocorrencia = reader.IsDBNull(idxOcorrencia) ? "" : reader.GetString(idxOcorrencia);
+                        }
+
+                        consulta = BD.obtem_descricao_tabela_t_codigo_descricao(Global.Cte.GRUPO_T_CODIGO_DESCRICAO__OCORRENCIAS_EM_PEDIDOS__TIPO_OCORRENCIA, reader.IsDBNull(idxTipoOcorrencia) ? "" : reader.GetString(idxTipoOcorrencia));
+                        
+                        if (!string.IsNullOrEmpty(consulta))
+                        {
+                            _novo.TipoOcorrencia = consulta;
+
+                        }
+                        else
+                        {
+                            _novo.TipoOcorrencia = reader.IsDBNull(idxTipoOcorrencia) ? "" : reader.GetString(idxTipoOcorrencia);
+                        } */
+                        
+                        ListaOcorrenciasStatus.Add(_novo);
+                    }
+                }
+                finally
+                {
+                    reader.Close();
+                }
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            return ListaOcorrenciasStatus.ToArray();
+        }
+        
+
+    }
+}
