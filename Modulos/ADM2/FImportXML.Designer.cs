31c5a84e54a2a7f52aec284c1230165fbf45fc29
Luiz Xavier <luiz.xavier@tropikal.com.br>
2021-01-04 15:24:19 -0300

    atualizar datas emissao das notas importadas via XML

--- Modulos/ADM2/FImportXML.Designer.cs
+++ Modulos/ADM2/FImportXML.Designer.cs
@@ -29,16 +29,40 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FImportXML));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle16 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle13 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle14 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle15 = new System.Windows.Forms.DataGridViewCellStyle();
             this.lblTitulo = new System.Windows.Forms.Label();
             this.gboxMsgErro = new System.Windows.Forms.GroupBox();
             this.lbErro = new System.Windows.Forms.ListBox();
             this.gboxMensagensInformativas = new System.Windows.Forms.GroupBox();
             this.lbMensagem = new System.Windows.Forms.ListBox();
             this.btnAtualizaDatas = new System.Windows.Forms.Button();
+            this.gboxDados = new System.Windows.Forms.GroupBox();
+            this.lblTotalRegistros = new System.Windows.Forms.Label();
+            this.lblTitTotalRegistros = new System.Windows.Forms.Label();
+            this.grdDados = new System.Windows.Forms.DataGridView();
+            this.colCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colEX = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTabela = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colAliqNac = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colAliqImp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDescricao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LblPeriodoEntradaNFde = new System.Windows.Forms.Label();
+            this.dtpDataEntradaIni = new System.Windows.Forms.DateTimePicker();
+            this.dtpDataEntradaFim = new System.Windows.Forms.DateTimePicker();
+            this.LblPeriodoEntradaNFate = new System.Windows.Forms.Label();
             this.pnBotoes.SuspendLayout();
             this.pnCampos.SuspendLayout();
             this.gboxMsgErro.SuspendLayout();
             this.gboxMensagensInformativas.SuspendLayout();
+            this.gboxDados.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdDados)).BeginInit();
             this.SuspendLayout();
             // 
             // pnBotoes
@@ -55,6 +79,11 @@
             // 
             // pnCampos
             // 
+            this.pnCampos.Controls.Add(this.dtpDataEntradaFim);
+            this.pnCampos.Controls.Add(this.LblPeriodoEntradaNFate);
+            this.pnCampos.Controls.Add(this.dtpDataEntradaIni);
+            this.pnCampos.Controls.Add(this.LblPeriodoEntradaNFde);
+            this.pnCampos.Controls.Add(this.gboxDados);
             this.pnCampos.Controls.Add(this.gboxMsgErro);
             this.pnCampos.Controls.Add(this.gboxMensagensInformativas);
             this.pnCampos.Controls.Add(this.lblTitulo);
@@ -123,6 +152,183 @@
             this.btnAtualizaDatas.TabStop = false;
             this.btnAtualizaDatas.UseVisualStyleBackColor = true;
             // 
+            // gboxDados
+            // 
+            this.gboxDados.Controls.Add(this.lblTotalRegistros);
+            this.gboxDados.Controls.Add(this.lblTitTotalRegistros);
+            this.gboxDados.Controls.Add(this.grdDados);
+            this.gboxDados.Location = new System.Drawing.Point(7, 82);
+            this.gboxDados.Name = "gboxDados";
+            this.gboxDados.Size = new System.Drawing.Size(994, 173);
+            this.gboxDados.TabIndex = 10;
+            this.gboxDados.TabStop = false;
+            this.gboxDados.Text = "Dados do Arquivo";
+            // 
+            // lblTotalRegistros
+            // 
+            this.lblTotalRegistros.AutoSize = true;
+            this.lblTotalRegistros.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalRegistros.Location = new System.Drawing.Point(108, 154);
+            this.lblTotalRegistros.Name = "lblTotalRegistros";
+            this.lblTotalRegistros.Size = new System.Drawing.Size(28, 13);
+            this.lblTotalRegistros.TabIndex = 2;
+            this.lblTotalRegistros.Text = "999";
+            // 
+            // lblTitTotalRegistros
+            // 
+            this.lblTitTotalRegistros.AutoSize = true;
+            this.lblTitTotalRegistros.Location = new System.Drawing.Point(20, 154);
+            this.lblTitTotalRegistros.Name = "lblTitTotalRegistros";
+            this.lblTitTotalRegistros.Size = new System.Drawing.Size(88, 13);
+            this.lblTitTotalRegistros.TabIndex = 1;
+            this.lblTitTotalRegistros.Text = "Total de registros";
+            // 
+            // grdDados
+            // 
+            this.grdDados.AllowUserToAddRows = false;
+            this.grdDados.AllowUserToDeleteRows = false;
+            this.grdDados.AllowUserToResizeColumns = false;
+            this.grdDados.AllowUserToResizeRows = false;
+            this.grdDados.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.grdDados.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.BottomCenter;
+            dataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grdDados.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle9;
+            this.grdDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdDados.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colCodigo,
+            this.colEX,
+            this.colTabela,
+            this.colAliqNac,
+            this.colAliqImp,
+            this.colDescricao});
+            dataGridViewCellStyle16.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle16.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle16.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle16.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle16.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle16.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle16.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.grdDados.DefaultCellStyle = dataGridViewCellStyle16;
+            this.grdDados.Location = new System.Drawing.Point(15, 19);
+            this.grdDados.MultiSelect = false;
+            this.grdDados.Name = "grdDados";
+            this.grdDados.ReadOnly = true;
+            this.grdDados.RowHeadersVisible = false;
+            this.grdDados.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.grdDados.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.grdDados.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grdDados.ShowEditingIcon = false;
+            this.grdDados.Size = new System.Drawing.Size(965, 129);
+            this.grdDados.StandardTab = true;
+            this.grdDados.TabIndex = 0;
+            // 
+            // colCodigo
+            // 
+            this.colCodigo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopLeft;
+            this.colCodigo.DefaultCellStyle = dataGridViewCellStyle10;
+            this.colCodigo.HeaderText = "Código";
+            this.colCodigo.MinimumWidth = 100;
+            this.colCodigo.Name = "colCodigo";
+            this.colCodigo.ReadOnly = true;
+            // 
+            // colEX
+            // 
+            this.colEX.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            this.colEX.DefaultCellStyle = dataGridViewCellStyle11;
+            this.colEX.HeaderText = "EX";
+            this.colEX.MinimumWidth = 60;
+            this.colEX.Name = "colEX";
+            this.colEX.ReadOnly = true;
+            this.colEX.Width = 60;
+            // 
+            // colTabela
+            // 
+            this.colTabela.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            this.colTabela.DefaultCellStyle = dataGridViewCellStyle12;
+            this.colTabela.HeaderText = "Tabela";
+            this.colTabela.MinimumWidth = 80;
+            this.colTabela.Name = "colTabela";
+            this.colTabela.ReadOnly = true;
+            this.colTabela.Width = 80;
+            // 
+            // colAliqNac
+            // 
+            this.colAliqNac.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            dataGridViewCellStyle13.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopRight;
+            dataGridViewCellStyle13.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.colAliqNac.DefaultCellStyle = dataGridViewCellStyle13;
+            this.colAliqNac.HeaderText = "Alíquota Nac";
+            this.colAliqNac.MinimumWidth = 110;
+            this.colAliqNac.Name = "colAliqNac";
+            this.colAliqNac.ReadOnly = true;
+            this.colAliqNac.Width = 110;
+            // 
+            // colAliqImp
+            // 
+            this.colAliqImp.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            dataGridViewCellStyle14.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopRight;
+            dataGridViewCellStyle14.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.colAliqImp.DefaultCellStyle = dataGridViewCellStyle14;
+            this.colAliqImp.HeaderText = "Alíquota Imp";
+            this.colAliqImp.MinimumWidth = 110;
+            this.colAliqImp.Name = "colAliqImp";
+            this.colAliqImp.ReadOnly = true;
+            this.colAliqImp.Width = 110;
+            // 
+            // colDescricao
+            // 
+            this.colDescricao.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle15.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopLeft;
+            dataGridViewCellStyle15.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.colDescricao.DefaultCellStyle = dataGridViewCellStyle15;
+            this.colDescricao.HeaderText = "Descrição";
+            this.colDescricao.MinimumWidth = 150;
+            this.colDescricao.Name = "colDescricao";
+            this.colDescricao.ReadOnly = true;
+            // 
+            // LblPeriodoEntradaNFde
+            // 
+            this.LblPeriodoEntradaNFde.AutoSize = true;
+            this.LblPeriodoEntradaNFde.Location = new System.Drawing.Point(15, 51);
+            this.LblPeriodoEntradaNFde.Name = "LblPeriodoEntradaNFde";
+            this.LblPeriodoEntradaNFde.Size = new System.Drawing.Size(166, 13);
+            this.LblPeriodoEntradaNFde.TabIndex = 11;
+            this.LblPeriodoEntradaNFde.Text = "Período de Entrada das NFs:  de ";
+            // 
+            // dtpDataEntradaIni
+            // 
+            this.dtpDataEntradaIni.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDataEntradaIni.Location = new System.Drawing.Point(187, 51);
+            this.dtpDataEntradaIni.Name = "dtpDataEntradaIni";
+            this.dtpDataEntradaIni.Size = new System.Drawing.Size(101, 20);
+            this.dtpDataEntradaIni.TabIndex = 12;
+            // 
+            // dtpDataEntradaFim
+            // 
+            this.dtpDataEntradaFim.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDataEntradaFim.Location = new System.Drawing.Point(328, 51);
+            this.dtpDataEntradaFim.Name = "dtpDataEntradaFim";
+            this.dtpDataEntradaFim.Size = new System.Drawing.Size(101, 20);
+            this.dtpDataEntradaFim.TabIndex = 14;
+            // 
+            // LblPeriodoEntradaNFate
+            // 
+            this.LblPeriodoEntradaNFate.AutoSize = true;
+            this.LblPeriodoEntradaNFate.Location = new System.Drawing.Point(294, 51);
+            this.LblPeriodoEntradaNFate.Name = "LblPeriodoEntradaNFate";
+            this.LblPeriodoEntradaNFate.Size = new System.Drawing.Size(28, 13);
+            this.LblPeriodoEntradaNFate.TabIndex = 13;
+            this.LblPeriodoEntradaNFate.Text = " até ";
+            // 
             // FImportXML
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -134,8 +340,12 @@
             this.Shown += new System.EventHandler(this.FImportXML_Shown);
             this.pnBotoes.ResumeLayout(false);
             this.pnCampos.ResumeLayout(false);
+            this.pnCampos.PerformLayout();
             this.gboxMsgErro.ResumeLayout(false);
             this.gboxMensagensInformativas.ResumeLayout(false);
+            this.gboxDados.ResumeLayout(false);
+            this.gboxDados.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdDados)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -149,5 +359,19 @@
         private System.Windows.Forms.GroupBox gboxMensagensInformativas;
         private System.Windows.Forms.ListBox lbMensagem;
         private System.Windows.Forms.Button btnAtualizaDatas;
+        private System.Windows.Forms.GroupBox gboxDados;
+        private System.Windows.Forms.Label lblTotalRegistros;
+        private System.Windows.Forms.Label lblTitTotalRegistros;
+        private System.Windows.Forms.DataGridView grdDados;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCodigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEX;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTabela;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAliqNac;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAliqImp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDescricao;
+        private System.Windows.Forms.Label LblPeriodoEntradaNFde;
+        private System.Windows.Forms.DateTimePicker dtpDataEntradaFim;
+        private System.Windows.Forms.Label LblPeriodoEntradaNFate;
+        private System.Windows.Forms.DateTimePicker dtpDataEntradaIni;
     }
 }
